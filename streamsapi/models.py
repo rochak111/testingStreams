f26f73fb4bd9125ead73c7945528ddf4bfd2eea1
Rochak <rochakbhattarai11@gmail.com>
2022-08-23 14:41:06 +0530

    Fix Tables

--- streamsapi/models.py
+++ streamsapi/models.py
@@ -4,14 +4,14 @@ from django.db import models
 # Create your models here.
 class Matches(models.Model):
     title=models.CharField(max_length=150)
-    match_id=models.IntegerField()
+    # match_id=models.IntegerField()
     description=models.TextField()
     embed_code=models.URLField()
     status=models.BooleanField(default=True)
 
     class Meta:
         verbose_name_plural = 'Matches'
-        ordering=['-match_id']
+        ordering=['-id']
     
     def __str__(self):
         return self.title
